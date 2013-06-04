e520738f8eb8e14a137b4d8adc414db450b84b75
jinpeng <jinpeng.dong@gmail.com>
2013-06-04 12:08:22 +0800

    recipes 01-05

---
+++ recipe05_runner.py
@@ -0,0 +1,10 @@
+__author__ = 'jinpeng'
+
+if __name__ == "__main__":
+    import unittest
+    from recipe05 import *
+
+    suite1 = unittest.TestLoader().loadTestsFromTestCase(RomanNumeralConverterTest)
+    suite2 = unittest.TestLoader().loadTestsFromTestCase(RomanNumeralComboTest)
+    suite = unittest.TestSuite([suite1, suite2])
+    unittest.TextTestRunner(verbosity=2).run(suite)
